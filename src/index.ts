ba0b359842667275bbd239028d79b67adf3ef5ac
Yaman Ozakin <yaman.oezakin@atlantic-ventures.com>
2025-06-18 18:51:58 +0200

    add exports, bump up the version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import DirectedAcyclicGraph from './directedAcyclicGraph'
 import DirectedGraph from './directedGraph'
 import Graph, { Edge } from './graph'
+export * from './errors'
 
 export { Graph, DirectedGraph, Edge, DirectedAcyclicGraph }
